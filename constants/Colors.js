c957ed1d8af365b06f9959e61f6dbf91e145acb0
E.S <extrin13@gmail.com>
2019-08-08 10:06:50 +0300

    Get rid of tab bar

--- constants/Colors.js
+++ constants/Colors.js
@@ -1,14 +1,13 @@
-const tintColor = '#2f95dc';
+const tintColor = "#2f95dc";
 
 export default {
   tintColor,
-  tabIconDefault: '#ccc',
-  tabIconSelected: tintColor,
-  tabBar: '#fefefe',
-  errorBackground: 'red',
-  errorText: '#fff',
-  warningBackground: '#EAEB5E',
-  warningText: '#666804',
+  iconDefault: "#ccc",
+  iconSelected: tintColor,
+  errorBackground: "red",
+  errorText: "#fff",
+  warningBackground: "#EAEB5E",
+  warningText: "#666804",
   noticeBackground: tintColor,
-  noticeText: '#fff',
+  noticeText: "#fff"
 };
