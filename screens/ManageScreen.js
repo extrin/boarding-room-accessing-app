2930c21c5e704e78dbd9a5fb29f77963328c7bd4
E.S <extrin13@gmail.com>
2019-08-08 17:34:28 +0300

    Change title, remove webview margin

--- screens/ManageScreen.js
+++ screens/ManageScreen.js
@@ -1,18 +1,10 @@
 import React from "react";
-import { View } from "react-native";
 import { WebView } from "react-native-webview";
 
 export default function ManageScreen() {
-  return (
-    <View>
-      <WebView
-        source={{ uri: "http://10.2.0.3:1880/ui/#!/0" }}
-        style={{ marginTop: 20 }}
-      />
-    </View>
-  );
+  return <WebView source={{ uri: "http://10.2.0.3:1880/ui/#!/0" }} />;
 }
 
 ManageScreen.navigationOptions = {
-  title: "Управление переговорной комнатой"
+  title: "Управление комнатой"
 };
