044a39b4d20faa5f7ef8e9733b8605ace82ab0b8
E.S <extrin13@gmail.com>
2019-08-09 10:56:27 +0300

    Add loading indicator

--- screens/ManageScreen.js
+++ screens/ManageScreen.js
@@ -1,10 +1,28 @@
 import React from "react";
 import { WebView } from "react-native-webview";
+import { ActivityIndicator, Text } from "react-native";
+import Colors from "../constants/Colors";
 
-export default function ManageScreen() {
-  return <WebView source={{ uri: "http://10.2.0.3:1880/ui/#!/0" }} />;
+export default class ManageScreen extends React.Component {
+  render() {
+    return (
+      <WebView
+        textZoom={100}
+        startInLoadingState={true}
+        source={{ uri: "http://10.2.0.3:1880/ui/#!/0" }}
+        renderError={errorName => <Error errorName={errorName} />}
+        renderLoading={() => (
+          <ActivityIndicator size="large" color={Colors.tintColor} />
+        )}
+      />
+    );
+  }
 }
 
 ManageScreen.navigationOptions = {
   title: "Управление комнатой"
 };
+
+function Error(props) {
+  return <Text>{props.errorName}</Text>;
+}
