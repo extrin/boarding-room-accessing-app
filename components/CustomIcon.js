458bedc3e885f368ef4683698f9790193e65d018
E.S <extrin13@gmail.com>
2019-08-16 14:08:07 +0300

    Apply icons

--- components/CustomIcon.js
+++ components/CustomIcon.js
@@ -1,13 +1,6 @@
 import React from 'react';
-import {Icon} from 'react-native-elements';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 export default function CustomIcon(props) {
-  return (
-    <Icon
-      name={props.name}
-      size={props.size}
-      color={props.color}
-      type="material-community"
-    />
-  );
+  return <Icon name={props.name} size={props.size} color={props.color} />;
 }
